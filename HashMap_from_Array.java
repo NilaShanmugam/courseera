5d64152d3ec334522e504adb1bc8e26c3be82c3b
NilaShanmugam <59618634+NilaShanmugam@users.noreply.github.com>
2021-02-25 15:54:00 +0530

    Create HashMap_from_Array.java

---
+++ HashMap_from_Array.java
@@ -0,0 +1,20 @@
+HashMap <Integer, String> hm = new HashMap<Integer, String>();
+
+
+// arr[i] => Key
+
+for (int i =0;i<arrr.length;i++){
+
+// First occurence
+  if(hm.get(arr[i]==null){
+    hm.put(arr[i],1);
+  }
+     else{
+       hm.put(arr[i],i);
+     }
+
+//Iteration
+for(Map.Entry m : hm.emptySet()){
+  System.out.println(m.getKey(),m.getValue());
+}
+}
