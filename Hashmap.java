9a10c1de91217a78e6c1f0e3b10c7431a199e0c1
NilaShanmugam <59618634+NilaShanmugam@users.noreply.github.com>
2021-02-25 16:06:54 +0530

    Update and rename HashMap_from_Array.java to Hashmap.java

--- Hashmap.java
+++ Hashmap.java
@@ -1,16 +1,21 @@
+
+//Java program to create HashMap from an array 
+// by taking the elements as Keys and 
+// the frequencies as the Values 
+
 HashMap <Integer, String> hm = new HashMap<Integer, String>();
 
 
 // arr[i] => Key
 
 for (int i =0;i<arrr.length;i++){
-
-// First occurence
+int count = hm.get(arr[i]);
+// First occurence of the element (Eg: 2 is occuring for 1st time)
   if(hm.get(arr[i]==null){
     hm.put(arr[i],1);
   }
      else{
-       hm.put(arr[i],i);
+       hm.put(arr[i],++c);
      }
 
 //Iteration
